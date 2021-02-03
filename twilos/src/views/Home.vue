1cb93a5120aa93f539d7ddfaad3b93f90a2e3bec
Annaelle <annaellejaffrelot@outlook.fr>
2021-02-03 09:31:34 +0100

    views

---
+++ twilos/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Timeline/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Timeline from '@/components/timeline.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Timeline
+  }
+}
+</script>
