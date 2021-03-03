400443cbe401bf2d67dba0b8ca5554f66536c9fd
Annaelle <annaellejaffrelot@outlook.fr>
2021-03-03 13:56:05 +0100

    Keycloack

--- twilos/src/main.js
+++ twilos/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import * as Keycloak from 'keycloak-js';
 
 Vue.config.productionTip = false
 
@@ -8,3 +9,46 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+
+
+
+let initOptions = {
+  url: 'http://localhost:8080/auth', realm: 'twilos', clientId: 'twilos', onLoad:'login-required'
+}
+
+let keycloak = Keycloak(initOptions);
+
+keycloak.init({ onLoad: initOptions.onLoad }).success((auth) =>{
+    
+    if(!auth) {
+      window.location.reload();
+    } else {
+      Vue.$log.info("Authenticated");
+    }
+ 
+    new Vue({
+      render: h => h(App),
+    }).$mount('#app')
+  
+
+    localStorage.setItem("vue-token", keycloak.token);
+    localStorage.setItem("vue-refresh-token", keycloak.refreshToken);
+
+    setInterval(() =>{
+      keycloak.updateToken(70).success((refreshed)=>{
+        if (refreshed) {
+          Vue.$log.debug('Token refreshed'+ refreshed);
+        } else {
+          Vue.$log.warn('Token not refreshed, valid for '
+          + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
+        }
+      }).error(()=>{
+          Vue.$log.error('Failed to refresh token');
+      });
+
+
+    }, 60000)
+
+}).error(() =>{
+  Vue.$log.error("Authenticated Failed");
+});
